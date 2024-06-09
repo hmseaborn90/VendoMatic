b46dc62018b19be1b9a431cb4997cfde8a845b35
Harrison <hmseaborn90.gmail.com>
2024-06-08 18:20:15 -0700

    Created logger file to log messages to logs directory in root of folder

--- src/main/java/com/techelevator/util/Logger.java
+++ src/main/java/com/techelevator/util/Logger.java
@@ -1,5 +1,29 @@
 package com.techelevator.util;
 
+import com.techelevator.exceptions.LoggerException;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Logger {
-    //TODO create log method
+    private static PrintWriter logWriter;
+
+    public static void log(String message) throws LoggerException{
+        File logs = new File("logs");
+        logs.mkdir();
+        LocalDateTime date = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        if(logWriter == null){
+            try{
+                String destinationPath = "logs/";
+                logWriter = new PrintWriter(new FileWriter(destinationPath + "log.txt", true));
+            } catch (IOException e){
+                throw new LoggerException("error wrtiteng to log file" + e.getMessage());
+            }
+        }
+    }
 }
