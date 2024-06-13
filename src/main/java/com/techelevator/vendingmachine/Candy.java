cd01dc93929d8f414060ddd5d7cda7ba3446b91e
JORDIN <jordincole7502gmail.com>
2024-06-13 01:41:26 -0400

    superclasses

--- src/main/java/com/techelevator/vendingmachine/Candy.java
+++ src/main/java/com/techelevator/vendingmachine/Candy.java
@@ -1,4 +1,7 @@
 package com.techelevator.vendingmachine;
 
 public class Candy extends Product {
+    public Candy (String slotLocation, String productName, double productPrice, String productType) {
+       super(slotLocation, productName, productPrice, productType);
+    }
 }
--- src/main/java/com/techelevator/vendingmachine/Drink.java
+++ src/main/java/com/techelevator/vendingmachine/Drink.java
@@ -1,4 +1,7 @@
 package com.techelevator.vendingmachine;
 
 public class Drink extends Product{
+    public Drink (String slotLocation, String productName, double productPrice, String productType) {
+    super(slotLocation, productName, productPrice, productType);
+}
 }
