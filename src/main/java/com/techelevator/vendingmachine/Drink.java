c5adb184aae125a2ed4c30319dca911554d03529
Harrison <hmseaborn90.gmail.com>
2024-06-14 13:16:30 -0700

    added toString methods on product children

--- src/main/java/com/techelevator/vendingmachine/Drink.java
+++ src/main/java/com/techelevator/vendingmachine/Drink.java
@@ -1,7 +1,12 @@
 package com.techelevator.vendingmachine;
 
-public class Drink extends Product{
-    public Drink (String slotLocation, String productName, double productPrice, String productType) {
-    super(slotLocation, productName, productPrice, productType);
-}
+public class Drink extends Product {
+    public Drink(String slotLocation, String productName, double productPrice, String productType) {
+        super(slotLocation, productName, productPrice, productType);
+    }
+
+    @Override
+    public String toString(){
+        return "Glug Glug, Yum!";
+    }
 }
